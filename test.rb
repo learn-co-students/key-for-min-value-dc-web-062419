de27ff59aa39eb4689101bd782d603f2925b0f14
Luke O'Malley <lukeomalley95@gmail.com>
2019-04-15 21:56:01 +0000

    Done.

--- test.rb
+++ test.rb
@@ -1,6 +1,3 @@
-# prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
-
 def key_for_min_value(name_hash)
   minValue = nil
   minKey = nil
@@ -20,3 +17,5 @@ def key_for_min_value(name_hash)
   end
   return minKey
 end
+
+puts key_for_min_value({:blake => 500, :ashley => 2, :adam => 1})
